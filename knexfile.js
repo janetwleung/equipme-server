8384b2af43bdd42bf9aa387e3e961915e72d5483
Janet Leung <leung.janetw@hotmail.com>
2022-09-14 16:34:33 -0400

    First commit

---
+++ knexfile.js
@@ -0,0 +1,10 @@
+module.exports = {
+  client: "capstone",
+  connection: {
+    host: "127.0.0.1",
+    database: "EquipMe",
+    user: "root",
+    password: "rootroot",
+    charset: "utf8"
+  }
+};
