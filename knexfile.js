91714ceb12c86d755beda1e290b2f527ee598570
Janet Leung <leung.janetw@hotmail.com>
2022-09-21 15:19:39 -0400

    Creates migrations folder and seeds folder. Created tables using migration

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,5 @@
 module.exports = {
-  client: "capstone",
+  client: "mysql2",
   connection: {
     host: "127.0.0.1",
     database: "EquipMe",
---
+++ migrations/20220921182948_create_gloves_bats_cleats_sports_tables.js
@@ -0,0 +1,86 @@
+exports.up = function (knex) {
+    return knex.schema
+        .createTable('brands', (table) => {
+            table.increments('id').primary();
+            table.string("brand").notNullable();
+        })
+        .createTable('gloves', (table) => {
+            table.increments('id').primary();
+            table.string("brand").notNullable();
+            table.string("category").notNullable();
+            table.string('name').notNullable();
+            table.string("image1").notNullable();
+            table.string("image2").notNullable();
+            table.string("price").notNullable();
+            table.string("size").notNullable();
+            table.string("pocket").notNullable();
+            table.string("color").notNullable();
+            table.string("position").notNullable();
+            table.string("sport").notNullable();
+            table.string("left").notNullable();
+            table.string("right").notNullable();
+            table.string("whereToBy").notNullable();
+            table.string("id1").notNullable();
+            table
+                .integer('brand_id')
+                .unsigned()
+                .notNullable()
+                .references('id')
+                .inTable('brands')
+                .onUpdate('CASCADE')
+                .onDelete('CASCADE');
+        })
+        .createTable('bats', (table) => {
+            table.increments('id').primary();
+            table.string("brand").notNullable();
+            table.string("category").notNullable();
+            table.string('name').notNullable();
+            table.string("image1").notNullable();
+            table.string("image2").notNullable();
+            table.string("price").notNullable();
+            table.string("age").notNullable();
+            table.string("length").notNullable();
+            table.string("whereToBuy").notNullable();
+            table.string("id1").notNullable();
+            table
+                .integer('brand_id')
+                .unsigned()
+                .notNullable()
+                .references('id')
+                .inTable('brands')
+                .onUpdate('CASCADE')
+                .onDelete('CASCADE');
+        })
+        .createTable('cleats', (table) => {
+            table.increments('id').primary();
+            table.string("brand").notNullable();
+            table.string("category").notNullable();
+            table.string('name').notNullable();
+            table.string("image1").notNullable();
+            table.string("image2").notNullable();
+            table.string("price").notNullable();
+            table.string("id1").notNullable();
+            table
+                .integer('brand_id')
+                .unsigned()
+                .notNullable()
+                .references('id')
+                .inTable('brands')
+                .onUpdate('CASCADE')
+                .onDelete('CASCADE');
+        })
+        .createTable('sports', (table) => {
+            table.increments('id').primary();
+            table.string('sport').notNullable();
+            table.string("image").notNullable();
+            table.string("formInput1").notNullable();
+            table.string("formInput2").notNullable();
+            table.string("formInput3").notNullable();
+            table.string("formInput4").notNullable();
+        });
+  };
+
+  
+  exports.down = function (knex) {
+    return knex.schema.dropTable('gloves').dropTable('bats').dropTable('cleats').dropTable('sports').dropTable('brands');
+  };
