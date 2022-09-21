ba153a1b37093f4ff27e570f40304e6f793a97e3
Janet Leung <leung.janetw@hotmail.com>
2022-09-20 21:39:16 -0400

    Adds id to response data for get sports endpoint

--- routes/sportsRoute.js
+++ routes/sportsRoute.js
@@ -14,6 +14,7 @@ router.get("/sports", (req, res) => {
     const sportsList = sports.map(sport => {
         return (
             {
+                "id": sport.id,
                 "sport": sport.sport,
                 "image": sport.image,
                 "hoverImage": sport.hoverImage,
