0669f11c8158a104a20ef40493cf8976e9047f6b
Janet Leung <leung.janetw@hotmail.com>
2022-09-27 15:43:34 -0400

    Changes image urls from localhost to heroku server url

--- seed_data/sports.js
+++ seed_data/sports.js
@@ -2,8 +2,8 @@ module.exports = [
     {
         id: "1",
         sport: "Softball",
-        image: "http://localhost:8080/images/sports/softball.png",
-        hoverImage: "http://localhost:8080/images/sports/softball1.jpeg",
+        image: "https://equipmeserver.herokuapp.com/images/sports/softball.png",
+        hoverImage: "https://equipmeserver.herokuapp.com/images/sports/softball1.jpeg",
         formInput1: "Age",
         formInput2: "Position",
         formInput3: "Height",
@@ -12,8 +12,8 @@ module.exports = [
     {
         id: "2",
         sport: "Hockey",
-        image: "http://localhost:8080/images/sports/hockey.png",
-        hoverImage: "http://localhost:8080/images/sports/hockey1.jpeg",
+        image: "https://equipmeserver.herokuapp.com/images/sports/hockey.png",
+        hoverImage: "https://equipmeserver.herokuapp.com/images/sports/hockey1.jpeg",
         formInput1: "Age",
         formInput2: "Position",
         formInput3: "Height",
@@ -22,8 +22,8 @@ module.exports = [
     {
         id: "3",
         sport: "Lacrosse",
-        image: "http://localhost:8080/images/sports/lacrosse.png",
-        hoverImage: "http://localhost:8080/images/sports/lacrosse1.jpeg",
+        image: "https://equipmeserver.herokuapp.com/images/sports/lacrosse.png",
+        hoverImage: "https://equipmeserver.herokuapp.com/images/sports/lacrosse1.jpeg",
         formInput1: "Age",
         formInput2: "Position",
         formInput3: "Height",
