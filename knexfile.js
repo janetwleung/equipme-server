b61c1954722082250d8284ace2145152800837c6
Janet Leung <leung.janetw@hotmail.com>
2022-09-27 15:24:51 -0400

    Adds shortcuts to json file and adds variables to .env, updates knexfile to use .env variables

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,12 @@
+require("dotenv").config();
+
 module.exports = {
   client: "mysql2",
   connection: {
-    host: "127.0.0.1",
-    database: "EquipMe",
-    user: "root",
-    password: "rootroot",
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
     charset: "utf8"
   }
 };
