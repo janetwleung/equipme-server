2292e4a347da4749687047866af3786f9881fee9
Janet Leung <leung.janetw@hotmail.com>
2022-09-27 21:40:48 -0400

    Adds column to select from for cleats route

--- routes/cleatsRoute.js
+++ routes/cleatsRoute.js
@@ -1,32 +1,10 @@
 const knex = require("knex")(require("../knexfile"));
 const express = require("express");
 const router = express.Router();
-// const fs = require("fs");
 
-// function readCleats() {
-//     const cleatsFile = fs.readFileSync("./data/cleats.json");
-//     const cleatsData = JSON.parse(cleatsFile);
-//     return cleatsData;
-// }
-
-// GET endpoint for list of all cleats
 router.get("/cleats", (req, res) => {
-    // const cleats = readCleats();
-    // const cleatsList = cleats.map(cleat => {
-    //     return (
-    //         {
-    //             "id": cleat.id,
-    //             "brand": cleat.brand,
-    //             "category": cleat.category,
-    //             "name": cleat.name,
-    //             "price": cleat.price,
-    //             "description": cleat.description
-    //         }
-    //     );
-    // })
-    // res.status(200).json(cleatsList);
     knex   
-        .select("uuid", "brand_id", "brand", "category", "name", "image1", "price")
+        .select("uuid", "brand_id", "brand", "category", "name", "image1", "price", "metal")
         .from("cleats")
         .then(cleatsResponse => {
             res.status(200).json(cleatsResponse);
@@ -38,14 +16,6 @@ router.get("/cleats", (req, res) => {
 
 // GET endpoint for specific cleat
 router.get("/cleats/:cleatId", (req, res) => {
-    // const cleats = readCleats();
-    // const specificCleatData = cleats.find(cleat => cleat.id === req.params.cleatId);
-
-    // if (!specificCleatData) {
-    //     return res.status(404).json({error: "Cleat not found. Please enter a valid cleat ID."})
-    // }
-
-    // return res.status(200).json(specificCleatData);
     knex
         .select("*")
         .from("cleats")
